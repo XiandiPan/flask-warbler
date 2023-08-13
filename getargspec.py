733306a3c073c8be7191f82a6858d65f7a3a42e6
Xiandi Pan <cindypan1807@gmail.com>
2023-08-13 18:47:11 -0400

    python3 version adding .y file

--- getargspec.py
+++ getargspec.py
@@ -7,4 +7,7 @@ signature = inspect.signature(my_function)
 parameters = signature.parameters
 
 for name, param in parameters.items():
-    print(name, param.default)
+    if param.default != inspect.Parameter.empty:
+        print(name, param.default)
+    else:
+        print(name, 'No default value')
